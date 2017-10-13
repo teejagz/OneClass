245ad6964f03ebbd7e07c73f8922f075921e03fc
teejagz <omotayo_lanre@yahoo.com>
2017-10-13 16:49:26 -0400

    Add files via upload

---
+++ enrollment.sql
@@ -0,0 +1,4 @@
+select count(e.user_id) as enrolled_count,count(distinct c.name) as courses,count(distinct d.name) as departments,s.name as schools from public.courses as c 
+inner join public.departments as d on c.department_id=d.id
+inner join public.schools as s on s.id=d.school_id  right join public.enrollments as e on e.course_id= c.id 
+group by s.name
